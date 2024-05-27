f78f2048d3e0914a2d6c9dfd44eba743d7f17acd
Nitish Raj <nitishrajpandey0707@gmail.com>
2024-05-28 00:26:23 +0530

    add header and footer

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,8 @@
 import React from "react";
-import { Signup } from "./components/signup-alert-dialog";
 
 function Home() {
   return (
     <div>
-      <nav>
-        <ul>
-          <li>Signin</li>
-          <Signup />
-        </ul>
-      </nav>
       <h1>Home</h1>
     </div>
   );
