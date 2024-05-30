f214978a23442abd98fc39aed153f89e135b1e81
Abhishekranjan-01 <abhishekranjan7991@outlook.com>
2024-05-30 14:28:55 +0530

    new route added for Partner Signup

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/header/Header";
 import Home from "./pages/home/Home";
 import Footer from "./components/footer/Footer";
 import UserSignup from "./pages/signup/User-Signup/User-signup";
+import BasicInfromation from "./pages/signup/Partner-Signup/Basic-Information";
 
 function App() {
   return (
@@ -12,6 +13,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/user-signup" element={<UserSignup />} />
+        <Route
+          path="/partner-signup/basic+information"
+          element={<BasicInfromation />}
+        />
       </Routes>
       <Footer />
     </BrowserRouter>
